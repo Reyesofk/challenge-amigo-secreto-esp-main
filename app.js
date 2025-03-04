5b9819ea98179f4bd40524a7332f452aa42c4ee2
Reyesofk <oreyes2780@gmail.com>
2025-03-04 11:53:56 -0500

    Se actualiza el  Boton Añadir

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+function agregarAmigo(){
+    let nombreAmigo = document.getElementById('amigo').value;
+    
+    
+    console.log(nombreAmigo);
+    alert("Tocaste el Boton");
+    return;
+
+}
